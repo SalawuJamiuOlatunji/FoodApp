44da19bc6532d433546aa07ff43259b8d8ff67bf
SalawuJamiuOlatunji <67075464+SalawuJamiuOlatunji@users.noreply.github.com>
2025-10-21 20:37:53 +0000

    FoodDetail

--- FOODAPP/src/components/FoodDetail.jsx
+++ FOODAPP/src/components/FoodDetail.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 export default function FoodDetail({ foodData, foodId }) {
+  const URL =`https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "fecc52070aa84c30bb688facd8675232";
+  useEffect(()=>{
+   async function fetchFood(){
+      const res = await fetch(`${URL}?apikey=${API_KEY}`)
+      const data = await res.json()
+      console.log(data)
+    }
+    fetchFood()
+  },[]);
   return <div>FoodDetail{foodId}</div>;
 }
 
