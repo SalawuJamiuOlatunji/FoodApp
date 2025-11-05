958efb4f5e625887e58bfc16c731b9f528510217
SalawujamiuOlatunji <salawujamiuolatunjialade@gmail.com>
2025-11-05 04:13:23 -0800

    completed

--- FOODAPP/src/components/Item.jsx
+++ FOODAPP/src/components/Item.jsx
@@ -1,15 +1,23 @@
+import styles from "./Item.module.css";
 export default function Item({ item }) {
   return (
     <div>
-      <div>
-        <img
-          src={`https://spoonacular.com/cdn/ingredients_100x100/` + item.image}
-          alt=""
-        />
-        <h3>{item.name}</h3>
-        <h3>
-          {item.amount} {item.unit}
-        </h3>
+      <div className={Styles.itemContainer}>
+        <div className={Styles.imageContainer}>
+          <img
+            className={styles.image}
+            src={
+              `https://spoonacular.com/cdn/ingredients_100x100/` + item.image
+            }
+            alt=""
+          />
+        </div>
+        <div className={styles.nameContainer}>
+          <div className={styles.name}>{item.name}</div>
+          <div className={styles.amount}>
+            {item.amount} {item.unit}
+          </div>
+        </div>
       </div>
     </div>
   );
