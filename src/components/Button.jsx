92f67f6dfe25ea0eeffe06a06144e3c44d90b56e
SalawujamiuOlatunji <salawujamiuolatunjialade@gmail.com>
2025-12-13 03:55:49 -0800

    ALHAMDULILAH

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,12 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import styles from "./Button.module.css";
 
 export default function Button({ setBg }) {
+  const toggle = () => {
+    setBg((prev) => (prev == "black" ? "white" : "black"));
+  };
   return (
     <div>
-      <button className={styles.Button} onClick={() => setBg("black")}>
+      <button className={styles.Button} onClick={toggle}>
         <i class="bi bi-moon-fill"></i>
       </button>
     </div>
